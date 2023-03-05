21b1d3b7800ff203084879ad37df9688989f6ff9
Diego <dmgelvez@gmail.com>
2023-03-05 18:32:40 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,49 @@
 const seasons = document.querySelector('#seasons');
-const caps = document.querySelector('#caps');
-const btn = document.querySelector('#randonbtn');
-const formulario = document.querySelector('#formulario');
-const resultado = document.querySelector('#resultado');
+const episodes = document.querySelector('#episodes');
+const btn = document.querySelector('#submitbtn');
+const form = document.querySelector('#form');
+const result = document.querySelector('#result');
 
-btn.addEventListener('click', math );
+btn.addEventListener('click', getEpisode );
 
-function math() {
+function getEpisode() {
    
-    if (seasons.value == '' || caps.value =='' ) {
-        mostrarAlerta('All fields must be complete');
+    if (seasons.value == '' || episodes.value =='' ) {
+        showAlert('All fields must be complete');
         return;
 
     }
 
-   const seasonsRandom = getRandomIntInclusive(1, parseInt(seasons.value))
-   const capRandom = getRandomIntInclusive(1, parseInt(caps.value))
+   const seasonRandom = getRandomNumber(1, parseInt(seasons.value))
+   const episodecapRandom = getRandomNumber(1, parseInt(episodes.value))
 
-    resultado.innerHTML = `
+    result.innerHTML = `
     <h4>You should watch</h4>
-    <p><strong>Season:</strong> ${seasonsRandom}<br>
-    <strong>Episode:</strong> ${capRandom}</p>`
+    <p><strong>Season:</strong> ${seasonRandom}<br>
+    <strong>Episode:</strong> ${episodecapRandom}</p>`
 }
 
-// Muestra una alerta de error o correcto
-function mostrarAlerta(mensaje) {
-    const alerta = document.querySelector('.bg-red-100');
-    if(!alerta) {
-        const alerta = document.createElement('p');
 
-        alerta.classList.add('alert', 'alert-danger', 'd-flex', 'align-items-center', 'gap-2' );
+function showAlert(mensaje) {
+    const alert = document.querySelector('.bg-red-100');
+    if(!alert) {
+        const alert = document.createElement('p');
+
+        alert.classList.add('alert', 'alert-danger', 'd-flex', 'align-items-center', 'gap-2' );
     
-        alerta.innerHTML = `
+        alert.innerHTML = `
             <strong>Error!</strong> ${mensaje}
         `;
     
-        formulario.appendChild(alerta);
+        form.appendChild(alert);
     
         setTimeout(() => {
-            alerta.remove();
+            alert.remove();
         }, 2000);
     }
 }
 
-function getRandomIntInclusive(min, max) {
+function getRandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
